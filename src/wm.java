7adb57af5032dc63835964aed70f928627b4232d
Thesin-01 <sithumishra76@gmail.com>
2026-01-16 12:40:51 +0530

    commited

---
+++ src/wm.java
@@ -0,0 +1,25 @@
+interface Motor {
+    int capacity = 5;
+    void run();
+    void consume();
+}
+
+class WashingMachine implements Motor {
+    public void run() {
+        System.out.println("Motor is running");
+    }
+
+    public void consume() {
+        System.out.println("Motor is consuming power");
+    }
+}
+
+public class wm {
+    public static void main(String[] args) {
+        WashingMachine wm = new WashingMachine();
+        wm.run();
+        wm.consume();
+        System.out.println("Capacity of the motor is " + Motor.capacity);
+    }
+}
+
